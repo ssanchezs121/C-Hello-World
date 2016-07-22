34ceecc44511feed987b8dcffd2d17df31ebf0f6
CMS <operator.cms@covidien.com>
2016-07-21 18:16:01 -0600

    Agregacion de Comentarios

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace Hello_World 
+using System.Threading.Tasks; //Utilizacion de otras libretas
+ 
+namespace Hello_World //Agrupar codigo
 {
+    
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Cualquier tipo de Texto
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World"); //Visualizacion
             Console.WriteLine("Press any Key to exit");
-            Console.ReadKey();
+            Console.ReadKey(); //Escribir
         }
     }
 }
